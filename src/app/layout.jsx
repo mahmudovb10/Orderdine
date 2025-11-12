7c8f78697958e87429e717bf64d42bc8a2f0def0
Bahodirxon <bohodirxon.mahmudoov@gmail.com>
2025-11-12 19:11:15 +0500

    Products have been imported. Errors have been fixed.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,27 +1,20 @@
-// src/app/layout.jsx (To'g'ri UI Layout)
-
 import { GlobalContextProvider } from "@/context/GlobalContext";
 import Link from "next/link";
 import "./globals.css";
-
-// ... metadata
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
       <body>
         <GlobalContextProvider>
-          {/* UI elementlar bu yerda tiklanadi */}
           <div className="main__layout min-h-screen flex flex-col">
             <header className="header bg-base-300 p-4 shadow-md">
-              {/* ... Header kontenti ... */}
-              <Link href="/dashboard">Dashboard</Link>
+              <Link href="#">Dashboard</Link>
             </header>
 
             <main className="main flex-grow p-4">{children}</main>
 
             <footer className="footer bg-neutral text-neutral-content p-4 mt-auto">
-              {/* ... Footer kontenti ... */}
               <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
             </footer>
           </div>
@@ -30,4 +23,3 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-// Eslatma: src/components/MainLayout.jsx faylini o'chirib yuborish tavsiya etiladi.
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,30 @@
 "use client";
+
+import { useFetch } from "@/lib/useFetch";
+
 export default function Home() {
+  const { data, isPending, error } = useFetch("/api.json");
   return (
     <div>
-      <h1>Commerce </h1>
+      <h1>Commerce</h1>
+      {data?.products.map((prod) => (
+        <div key={prod.id}>
+          <div className="card bg-base-100 w-96 shadow-sm">
+            <figure>
+              <img className="prodImage" src={prod.thumbnail} alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{prod.title}</h2>
+              <p>{prod.description}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">
+                  Sotib olish {prod.price} So'm
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
